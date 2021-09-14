024e250b0d533060ee64ce1b5eb094f7910b2383
Regine Mata <regine.mata.20@usjr.edu.ph>
2021-09-14 19:33:09 +0800

    Added Simple Calculator

---
+++ Simple Calculator/simpleCalculator.js
@@ -0,0 +1,36 @@
+        let n1=document.getElementById("num1");
+        let n2=document.getElementById("num2");
+        let res=document.getElementById("result")
+        
+        document.getElementById("add").addEventListener
+        ("click", function() {
+            
+            res.value= parseInt(n1.value)+parseInt(n2.value)
+
+        });
+
+        document.getElementById("sub").addEventListener
+        ("click", function() {
+            
+            res.value= parseInt(n1.value)-parseInt(n2.value)
+
+        });
+
+        document.getElementById("mul").addEventListener
+        ("click", function() {
+            
+            res.value= parseInt(n1.value)*parseInt(n2.value)
+
+        });
+        
+        document.getElementById("div").addEventListener
+        ("click", function() {
+            
+            res.value= parseInt(n1.value)/parseInt(n2.value)
+
+        });
+
+        function enableDarkMode() {
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+         }
