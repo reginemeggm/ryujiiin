c80459d8b0125e17fcf6155b78a1514e2ca68b59
Regine Mata <regine.mata.20@usjr.edu.ph>
2021-09-17 22:36:25 +0800

    Added To-Do List

---
+++ To-Do List/ToDoList.js
@@ -0,0 +1,52 @@
+var todoList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < todoList.length; i++) {
+  var span = document.createElement("SPAN");
+  var text = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(text);
+  todoList[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function addToDo() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("inputTask").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("taskList").appendChild(li);
+  }
+  document.getElementById("inputTask").value = "";
+
+  var span = document.createElement("SPAN");
+  var text = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(text);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
